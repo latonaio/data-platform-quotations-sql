50024f596fbc7ffe9e8deca6b5ffb27bf9fd4669
MiyuYokoyama0109 <miyu.y@latona.io>
2022-10-17 21:47:02 +0900

    release

---
+++ data-platform-quotations-sql-header-pdf-data.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `data_platform_quotations_header_pdf_data`       -- 新規追加
+(
+  `BusinessPartner`                int(10) NOT NULL,          -- 新規追加
+  `Quotation`                      int(10) NOT NULL,          -- 新規追加
+  `DocType`                        varchar(4) NOT NULL,       -- 新規追加
+  `DocID`                          int(20) NOT NULL,          -- 新規追加
+  `DocVersionID`                   int(4) NOT NULL,           -- 新規追加
+  `FileName`                       varchar(200) DEFAULT NULL, -- 新規追加
+    PRIMARY KEY (`BusinessPartner`, `Quotation`, `DocType`, `DocID`, `DocVersionID`)
+    CONSTRAINT `DataPlatformQuotationsHeaderPDFData_fk` FOREIGN KEY (`BusinessPartner`, `Quotation`) REFERENCES `data_platform_quotations_header_data` (`BusinessPartner`, `Quotation`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
