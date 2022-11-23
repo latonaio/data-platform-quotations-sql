7e7454552ae4b1d2a09979c8a4728cb17dd7873a
Yurino Oga <yurino.o@latona.io>
2022-11-23 23:27:50 +0900

    release

--- data-platform-quotations-sql-address-data.sql
+++ data-platform-quotations-sql-address-data.sql
@@ -1,18 +1,21 @@
-CREATE TABLE `data_platform_quotations_address_data`  -- 新規追加
+CREATE TABLE `data_platform_quotations_address_data`
 (
---  `BusinessPartner`         int(10) NOT NULL,           -- 新規追加
-    `Quotation`               int(16) NOT NULL,           -- 新規追加
-    `AddressID`               int(12) NOT NULL,           -- 新規追加
-    `PostalCode`              varchar(10) DEFAULT NULL,   -- 新規追加
-    `LocalRegion`             varchar(3) DEFAULT NULL,    -- 新規追加
-    `Country`                 varchar(3) DEFAULT NULL,    -- 新規追加
-    `District`                varchar(6) DEFAULT NULL,    -- 新規追加
-    `StreetName`              varchar(200) DEFAULT NULL,  -- 新規追加
-    `CityName`                varchar(200) DEFAULT NULL,  -- 新規追加
-    `Builiding`               varchar(100) DEFAULT NULL,  -- 新規追加
-    `Floor`                   int(4) DEFAULT NULL,        -- 新規追加
-    `Room`                    int(8) DEFAULT NULL,        -- 新規追加
+--  `BusinessPartner`         int(10) NOT NULL,
+    `Quotation`               int(16) NOT NULL,
+    `AddressID`               int(12) NOT NULL,
+    `PostalCode`              varchar(10) DEFAULT NULL,
+    `LocalRegion`             varchar(3) DEFAULT NULL,
+    `Country`                 varchar(3) DEFAULT NULL,
+    `District`                varchar(6) DEFAULT NULL,
+    `StreetName`              varchar(200) DEFAULT NULL,
+    `CityName`                varchar(200) DEFAULT NULL,
+    `Building`                varchar(100) DEFAULT NULL,
+    `Floor`                   int(4) DEFAULT NULL,
+    `Room`                    int(8) DEFAULT NULL,
+    
     PRIMARY KEY (`Quotation`, `AddressID`),
+
     CONSTRAINT `DataPlatformQuotationsAddressData_fk` FOREIGN KEY (`Quotation`) REFERENCES `data_platform_quotations_header_data` (`Quotation`)
+
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
